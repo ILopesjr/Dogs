ac902e3ba09afe82ca08ef95c76e1debc10ff11b
ilopesjr <ivanildo.lopes@orubetecnologia.com.br>
2022-01-21 07:24:44 -0300

    Login Routes

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,5 +1,19 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { LoginForm } from '../Login/LoginForm';
+import { LoginCreate } from '../Login/LoginCreate';
+import { LoginPasswordLost } from '../Login/LoginPasswordLost';
+import { LoginPasswordReset } from '../Login/LoginPasswordReset';
 
 export const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<LoginForm />} />
+        <Route path="criar" element={<LoginCreate />} />
+        <Route path="perdeu" element={<LoginPasswordLost />} />
+        <Route path="resetar" element={<LoginPasswordReset />} />
+      </Routes>
+    </div>
+  );
 };
